93748c19006117e989d93c389d264fb89e763d9c
Burak Çetin <burak.cetin@boun.edu.tr>
2022-01-10 15:00:32 +0300

    Initializing Codebase

---
+++ naive_bayes_test.py
@@ -0,0 +1,100 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+import pandas as pd
+import numpy as np
+import string
+import pickle
+
+features = ""
+
+
+
+#preprossing
+def document_cleaner(text, ps):
+    global stopwords
+    global features
+    text = text.lower()
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    text = text.translate(table)
+    doc_for_tf_idf= []
+    text = text.split()
+    for word in text:
+        stemmed_word = ps.stem(word)
+        if stemmed_word in features:
+            doc_for_tf_idf.append(stemmed_word)
+    doc_for_tf_idf = ' '.join(doc_for_tf_idf)
+            
+    return doc_for_tf_idf
+
+
+
+def main():
+    
+    #get title and abstract of test set and write to file
+    '''
+    global features
+    ps = PorterStemmer()
+    test_df = pd.read_csv(path, sep=',')
+    features = pickle.load(open("features.pkl", "rb")).tolist()
+    test_df['abstract'] = test_df['abstract'].map(lambda document : document_cleaner(document, ps))
+    test_df['title'] = test_df['title'].map(lambda document : document_cleaner(document, ps))
+    title_abstracts = test_df['title'] + " " + test_df['abstract']
+    
+    write_file = open("test_title_abstracts.pkl", "wb")
+    pickle.dump(title_abstracts, write_file)
+    '''
+
+    
+    #load test_title_abstract and test the model
+    path = 'BC7-LitCovid-Dev.csv'
+    test_df = pd.read_csv(path, sep=',')
+    pmid_ids = test_df['pmid']
+    features = pickle.load(open("features.pkl", "rb")).tolist()
+    test_title_abstracts = pickle.load(open("test_title_abstracts.pkl", "rb")).tolist()
+    tfidf = TfidfVectorizer()
+    test_X = tfidf.fit_transform(test_title_abstracts).toarray()
+    nb_model = pickle.load(open("naive_bayes_model.pkl", "rb"))
+    Y = nb_model.predict_proba(test_X)
+    Y = nb_model.predict(test_X)    
+    for i in range(len(Y)):
+        if Y[i] == 5:
+            print(i)
+    result = []
+    
+    
+    #append the predictions
+    for index, element in enumerate(Y):
+        test_Y = []
+        test_Y.append(pmid_ids[index])
+        test_Y.append(element[6])
+        test_Y.append(element[1])
+        test_Y.append(element[4])
+        test_Y.append(element[3])
+        test_Y.append(element[5])
+        test_Y.append(element[2])
+        test_Y.append(element[0])
+        result.append(test_Y)
+    
+    columns = ['PMID', 'Treatment', 'Diagnosis', 'Prevention', 'Mechanism', 'Transmission', 'Epidemic Forecasting', 'Case Report']
+    #dx = pd.DataFrame(result, columns = columns)
+    #dx.to_csv('prediction.csv', index=False)
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ naive_bayes_training.py
@@ -0,0 +1,119 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
+from sklearn.feature_selection import chi2, SelectKBest
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+import numpy as np
+import string
+import pickle
+
+
+
+
+
+stopwords = {'i', 'me', 'my', 'myself', 'we', 
+    'our', 'ours', 'ourselves', 'you', "you're", "you've", 
+    "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 
+    'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 
+    'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 
+    'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 
+    'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 
+    'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 
+    'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 
+    'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 
+    'for', 'with', 'about', 'against', 'between', 'into', 'through', 
+    'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 
+    'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 
+    'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 
+    'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 
+    'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 
+    'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 
+    'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 
+    'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', 
+    "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 
+    'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't", "covid-19", 
+    "covid", "covid19", "coronavirus", "covid19s", "covids"}
+
+
+
+def naive_bayes(X, Y):
+    multinomial_bayes = MultinomialNB()
+    bayes_model = multinomial_bayes.fit(X, Y)
+    #bayes_y = multinomial_bayes.predict(X_test)
+    return bayes_model
+
+
+def document_cleaner(text, ps):
+    global stopwords
+    text = text.lower()
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    text = text.translate(table)
+    doc_for_tf_idf= []
+    text = text.split()
+    for word in text:
+        stemmed_word = ps.stem(word)
+        if stemmed_word not in stopwords and word not in stopwords and not word.isnumeric():
+            doc_for_tf_idf.append(stemmed_word)
+    doc_for_tf_idf = ' '.join(doc_for_tf_idf)
+            
+    return doc_for_tf_idf
+
+
+def main():
+
+	#concatenating title and abstract and writing to a file
+    '''
+    ps = PorterStemmer()
+    df = pickle.load(open("df.pkl", "rb"))
+
+    df['abstract'] = df['abstract'].map(lambda document : document_cleaner(document, ps))
+    df['title'] = df['title'].map(lambda document : document_cleaner(document, ps))
+    title_abstracts = df['title'] + " " + df['abstract']
+    
+    write_file = open("title_abstracts.pkl", "wb")
+    pickle.dump(title_abstracts, write_file)
+    
+
+    '''
+	
+	
+    #get title_abstracts 
+    title_abstracts = pickle.load(open("title_abstracts.pkl", "rb")).tolist()
+    train_Y = pickle.load(open("train_Y.pkl", "rb"))
+    
+    tfidf = TfidfVectorizer(sublinear_tf=True, min_df = 5, norm='l2')
+    X = tfidf.fit_transform(title_abstracts).toarray()
+    
+	#feature selection
+    features = tfidf.get_feature_names_out()
+    #select best 100 feature
+    selector = SelectKBest(chi2, k=100)
+    train_X = selector.fit_transform(X, train_Y)
+    print(selector.get_feature_names_out(features))
+
+
+	#train naive bayes model
+    model = naive_bayes(train_X, train_Y)
+    write_file = open("naive_bayes_model.pkl", "wb")
+    pickle.dump(model, write_file) 
+   
+
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
